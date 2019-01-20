d8a230ad40a7b868d9df5562966ccce4ede10459
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2019-01-20 17:47:47 -0600

    First commit. Navbar added.

---
+++ src/components/general/Navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <Link to='/'><p>Inicio</p></Link>
+        <Link to='/solutions'><p>Nosotros</p></Link>
+        <Link to='/about'><p>Objetivos</p></Link>
+        <Link to='/products'><p>Newsletter</p></Link>
+        <Link to='/subsidiaries'><p>Registro</p></Link>
+        <Link to='/where'><p>Administración</p></Link>
+      </div>
+    )
+  }
+}
